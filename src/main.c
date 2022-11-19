7751c62427e590ed52f1d9c1a68cbbfa8ddc888c
longwatermelon <longwatermelon1@gmail.com>
2022-11-19 15:35:32 -0700

    restart working

--- src/main.c
+++ src/main.c
@@ -7,13 +7,22 @@
 #include <emscripten/emscripten.h>
 /* #include <SDL2/SDL_mixer.h> */
 
+struct Prog *g_prog;
+
 void emloop(void *arg)
 {
-    struct Prog *p = arg;
-    prog_mainloop(p);
+    prog_mainloop(g_prog);
 
-    if (!p->running)
+    if (!g_prog->running && !g_prog->restart)
         emscripten_cancel_main_loop();
+
+    if (!g_prog->running && g_prog->restart)
+    {
+        SDL_Window *w = g_prog->window;
+        SDL_Renderer *r = g_prog->rend;
+        prog_cleanup(g_prog);
+        g_prog = prog_init(w, r);
+    }
 }
 
 int main(int argc, char **argv)
@@ -28,20 +37,17 @@ int main(int argc, char **argv)
     SDL_Window *window = SDL_CreateWindow("Raycaster", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 800, SDL_WINDOW_SHOWN);
     SDL_Renderer *rend = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-    /* while (true) */
-    /* { */
-    struct Prog *p = prog_init(window, rend);
+    g_prog = prog_init(window, rend);
 
-    if (!p)
+    if (!g_prog)
     {
         fprintf(stderr, "Game failed to initialize, aborting\n");
         goto cleanup;
     }
 
-    emscripten_set_main_loop_arg(emloop, p, -1, 1);
+    emscripten_set_main_loop_arg(emloop, g_prog, -1, 1);
 
-    prog_cleanup(p);
-    /* } */
+    printf("Game end\n");
 
 cleanup:
     /* audio_cleanup(); */
